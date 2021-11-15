4f99538918084b7e4922e53109ffadbee98e720a
Joseph Siracusa <joeyqsa@outlook.com>
2021-11-15 00:05:51 -0500

    Added Basic Database Functionality

--- env.js
+++ env.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 
 module.exports = {
-    PORT: process.env.PORT
+    PORT: process.env.PORT,
+    CLEARDB_DATABASE_URL: process.env.CLEARDB_DATABASE_URL
 }
--- server.js
+++ server.js
@@ -2,36 +2,63 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const env = require('./env')
+const mysql = require('mysql')
 
 app.use(cors())
 app.use(express.json())
+
+const db = mysql.createPool({
+  host: "us-cdbr-east-04.cleardb.com",
+  user: "b26139898f7bfd",
+  password: "d848a9a7",
+  database: "heroku_26a6fd4771db623",
+  port: 3306,
+  connectionLimit: 20
+})
 
 app.get('/', (req, res) => {
   res.json("Server Initialized")
 })
 
 app.get('/users/:id', (req, res) => {
-  const id = req.params.id.toString()
-  
-  let charCodes = ""
+  const sql = `SELECT * FROM USERS WHERE id="${req.params.id.toString()}"`
 
-  for (let i = 0; i < id.length; i++)
-  {
-    charCodes += id.charCodeAt(i)
-  }
+  db.query(sql, (err, results) => {
+    try 
+    {
+      const result = results[0]
 
-  let user = {
-    id: id,
-    email: `${charCodes}@gmail.com`,
-    username: `${charCodes}`,
-    password: `password`,
-  }
+      const user = {
+        id: result.id,
+        email: result.email,
+        username: result.username,
+        password: result.password
+      }
 
-  res.json(user)
+      return res.json(user)
+    }
+
+    catch (error)
+    {
+      return res.json({error: "Bad Request Error"})
+    }
+  })
 })
 
 app.post('/users', (req, res) => {
-  res.json(req.body)
+  const sql = `INSERT INTO USERS VALUES("${req.body.id}", "${req.body.email}", "${req.body.username}", "${req.body.password}")`
+
+  db.query(sql, (err) => {
+    try 
+    {
+      return res.json({sucess: "Request Successful"})
+    }
+
+    catch (error)
+    {
+      return res.json({error: "Bad Request Error"})
+    }
+  })
 })
 
 app.listen(env.PORT, () => {
